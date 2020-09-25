0eb90a339625953b2861ef1bc03164dd58882648
Khairun Nessa Ayve <49992828+Ayve-178@users.noreply.github.com>
2020-09-26 01:37:09 +0600

    Create Addition_Subtraction_Multiplication_Division_Modulas.py

---
+++ Basics/Addition_Subtraction_Multiplication_Division_Modulas.py
@@ -0,0 +1,47 @@
+#Addition
+print(2+3)
+a = 5
+b = 3
+c = a+b
+print(c)
+
+#Subtraction
+print(10-2)
+a = 5
+b = 3
+c = a-b
+print(c)
+
+#Multiplication
+print(5*2)
+a = 5
+b = 3
+c = a*b
+print(c)
+
+#Division
+print(10/2)
+a = 5
+b = 2
+c = a/b
+d = a//b          #output will be the integer part only
+print(c)
+print(d)
+
+#Modulas
+print(10%3)
+a = 17
+b = 3
+c = a%b
+print(c)
+
+#Equation
+print(10+2-5*2)
+a = 12
+b = 3
+c = 13
+x = a*b-c
+print(x)
+
+#Happy Coding! ^_^ 
+
