d41964f8dbb3ea32e1e44925c48763388b25ae66
Khairun Nessa Ayve <49992828+Ayve-178@users.noreply.github.com>
2020-09-27 01:13:12 +0600

    Create 02. Addition_Subtraction_Multiplication_Division_Modulas_User_Input.py

---
+++ Basics/02. Addition_Subtraction_Multiplication_Division_Modulas_User_Input.py
@@ -0,0 +1,20 @@
+num1 = int(input('Enter the first number : '))
+num2 = int(input('Enter the second number : '))
+
+print("Addition")
+print(num1 + num2)
+
+print("Subtraction")
+print(num1 - num2)
+
+print("Multiplication")
+print(num1 * num2)
+
+print("Division")
+print(num1 / num2)
+
+print("Division (Integer part only)")
+print(num1 // num2)
+
+print("Modulas")
+print(num1%num2)
