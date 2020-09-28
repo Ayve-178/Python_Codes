e9230fd631f9a60a273833150e6dbcae4224bdfc
Khairun Nessa Ayve <49992828+Ayve-178@users.noreply.github.com>
2020-09-28 12:21:21 +0600

    Create 05. Strings_2.py

---
+++ Basics/05. Strings_2.py
@@ -0,0 +1,22 @@
+#What if we want to print 'Ayve's Laptop'?
+#print('Ayve's Laptop')
+#But this code will give you an error.
+print("Ayve's Laptop")
+#It will work. But what if we want to use single quotes.
+print('Ayve\'s Laptop')
+# \ considers the next character as a special character. So it doesn't give any error.
+
+#Suppose we want to print Ayve 3 times.
+print("AyveAyveAyve")
+print("Ayve"+"Ayve"+"Ayve")
+print(3*"Ayve")
+
+#what if a string contains a special character as a part of the string?
+print('c:\docs\new folder')
+#Here \n will give you a new line. But we don't want that.
+print(r'c:\docs\new folder')
+#r means raw. It is used to keep the raw string unchanged. r stops the role of a special character.
+
+
+#Happy Coding! ^_^
+
