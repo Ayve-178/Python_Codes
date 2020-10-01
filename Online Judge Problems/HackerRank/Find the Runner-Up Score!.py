b41c5788ed719867de573a8524b088669b53d541
Khairun Nessa Ayve <49992828+Ayve-178@users.noreply.github.com>
2020-10-01 20:02:03 +0600

    Create Find the Runner-Up Score!.py

---
+++ Online Judge Problems/HackerRank/Find the Runner-Up Score!.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+
+    listnew = []
+
+    for i in arr:
+        if i not in listnew:
+            listnew.append(i)
+    listnew.sort()
+    print(listnew[-2])
