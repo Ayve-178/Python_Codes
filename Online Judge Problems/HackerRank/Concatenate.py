009185f6ecf7495cdca6862656c5c6b2ed216083
Khairun Nessa Ayve <49992828+Ayve-178@users.noreply.github.com>
2020-11-01 18:23:38 +0600

    Create Concatenate.py

---
+++ Online Judge Problems/HackerRank/Concatenate.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+n, m, p = map(int,input().split())
+list1 = []
+list2 = []
+for i in range (n):
+    a = list(map(int,input().split()))
+    list1.append(a)
+for i in range (m):
+    b = list(map(int,input().split()))
+    list2.append(b)
+
+arr1 = np.array(list1)
+arr2 = np.array(list2)
+
+print(np.concatenate((arr1,arr2),axis = 0))
